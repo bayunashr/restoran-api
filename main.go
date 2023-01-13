e504dbdf2838274c027879c1cbe1e7ebe3c06788
Bayu Nashrullah <nashr.bayu77@gmail.com>
2023-01-13 17:08:39 +0700

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/bayunashr/restoran-api/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+}
+
+func main() {
+	app := gin.Default()
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
+	app.Run()
+}
